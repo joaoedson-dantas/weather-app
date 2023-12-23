4d50ce0497f791a34d42a41e20f43526865a6484
joaoedson <joaoedson.dantas@outlook.com>
2023-12-22 21:56:38 -0300

    feat: added the service to consume the Weather api

---
+++ src/app/modules/weather/services/weather.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  private apiKey = '05548647f85f0cb5cd6f7ea9a3c917d1';
+
+  constructor(private http: HttpClient) {}
+
+  getWeatherData(cityName: string): Observable<any> {
+    return this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=pt_br&appid=${this.apiKey}`,
+      {}
+    );
+  }
+}
+
+// Observable -> precisamos nos inscrever nesse observador para que seja possível ter acesso aos dados.
