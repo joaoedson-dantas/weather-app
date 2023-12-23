f0def1f6a698b34a67b27e6aa9e0cc8e5734e0d7
joaoedson <joaoedson.dantas@outlook.com>
2023-12-22 22:20:10 -0300

    feat: added the api response interface and consuming the service

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,7 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherDatas } from '../../../../models/interfaces/WeatherDatas';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
 })
-export class WeatherHomeComponent {}
+export class WeatherHomeComponent implements OnInit {
+  initialCityName = 'São Paulo';
+  weatherDatas!: WeatherDatas;
+
+  constructor(private weatherService: WeatherService) {}
+  ngOnInit(): void {
+    this.getWeatherData(this.initialCityName);
+  }
+
+  getWeatherData(cityName: string): void {
+    this.weatherService.getWeatherData(cityName).subscribe({
+      next: (res) => {
+        res && (this.weatherDatas = res);
+        console.log(this.weatherDatas);
+      },
+      error: (err) => console.log(err),
+    });
+  }
+}
