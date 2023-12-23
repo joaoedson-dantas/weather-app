7e5c6e0c1ba7e0de2ee271ae50170051fb673d13
joaoedson <joaoedson.dantas@outlook.com>
2023-12-22 21:38:15 -0300

    feat: created initial component weather

---
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-home',
+  templateUrl: './weather-home.component.html',
+})
+export class WeatherHomeComponent {}
